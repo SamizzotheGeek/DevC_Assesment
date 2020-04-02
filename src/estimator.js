13c5c779a228613fd2c1d8a21535de33ffc2e4fd
Samizzo <samizzographics@gmail.com>
2020-04-02 18:45:29 +0300

    first commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+/*const data = fetch('data.js')
+.then(res => res.json())
+.then((data) => {
+   data;
+}).catch(err => console.error(err));*/
+
+import data from './data.js'
+
+const covid19ImpactEstimator = (data) =>{
+    const {reportedCases} = data;
+    //const currentlyInfected = reportedCases * 10;
+
+    console.log(data);
+};
+covid19ImpactEstimator();
+console.log(data);
 
 export default covid19ImpactEstimator;
