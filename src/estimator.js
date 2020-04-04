7cf4a463c884804e0307af772870b18a318ceba4
Samizzo <samizzographics@gmail.com>
2020-04-04 06:44:36 +0300

    with corrections

--- src/estimator.js
+++ src/estimator.js
@@ -1,45 +1,45 @@
 // import data from './data.js'
 
 const covid19ImpactEstimator = (data) => {
-  const reportedCases = data.reportedCases;
-  let timeToElapse = data.timeToElapse;
-  let periodType = data.periodType;
-  let avgDailyIncomeInUSD = data.region.avgDailyIncomeInUSD;
-  let avgDailyIncomePopulation = data.reportedCases.avgDailyIncomePopulation;
-  let population = data.population;
-  let totalHospitalBeds = data.totalHospitalBeds;
+  const reportedCases = {data:reportedCases};
+  let timeToElapse = {data:timeToElapse};
+  const periodType = {data:periodType};
+  const avgDailyIncomeInUSD = {data:region.avgDailyIncomeInUSD};
+  const avgDailyIncomePopulation = {data:reportedCases.avgDailyIncomePopulation};
+  const population = {data:population};
+  const totalHospitalBeds = {data:totalHospitalBeds};
+  let factor;
 
-    //increment factor
-    switch(timeToElapse){
-        case periodType == 'days':
-            timeToElapse = timeToElapse;
-            factor = Math.pow(2, Math.floor((timeToElapse)/3));
-            break;
-        case periodType == 'weeks':
-            timeToElapse = timeToElapse * 7;
-            factor = Math.pow(2, Math.floor((timeToElapse)/3));
-            break;
-        case periodType == 'months':
-            timeToElapse = timeToElapse * 30;
-            factor = Math.pow(2, Math.floor((timeToElapse)/3));
-            break;
-        default:
-            timeToElapse = 0;
-            factor = Math.pow(2, Math.floor((timeToElapse)/3));
-            break;
+  // increment factor
+  switch (timeToElapse){
+    case periodType === 'days':
+        factor =2 ** Math.floor((timeToElapse) / 3);
+         break;
+    case periodType == 'weeks':
+        timeToElapse = timeToElapse * 7;
+        factor = 2 ** Math.floor((timeToElapse) / 3);
+        break;
+    case periodType == 'months':
+        timeToElapse = timeToElapse * 30;
+        factor = 2 ** Math.floor((timeToElapse) / 3);
+        break;
+    default:
+        timeToElapse = 0;
+        factor = 2 ** Math.floor((timeToElapse) / 3);
+        break;
     };
 
    
-    let output = {
-        data:{data},
-        impact:{
-            currentlyInfected: reportedCases * 10,
-            infectionsByRequestedTime: currentlyInfected * factor,
-            severeCasesByRequestedTime : Math.floor(infectionsByRequestedTime * 0.15),
-            hospitalBedsByRequestedTime : Math.floor((totalHospitalBeds * 0.35)) - severeCasesByRequesedtTime,
-            casesForICUByRequestedTime : Math.floor(0.05 * infectionsByRequestedTime),
-            casesForVentilatorsByRequestedTime : Math.floor(0.02 * infectionsByRequestedTime),
-            dollarsInFlight : infectionsByRequestedTime * avgDailyIncomeInUSD * avgDailyIncomePopulation * timeToElapse,
+let output = {
+  data:{data},
+  impact:{
+    currentlyInfected: reportedCases * 10,
+    infectionsByRequestedTime: currentlyInfected * factor,
+    severeCasesByRequestedTime : Math.floor(infectionsByRequestedTime * 0.15),
+    hospitalBedsByRequestedTime : Math.floor((totalHospitalBeds * 0.35)) - severeCasesByRequesedtTime,
+    casesForICUByRequestedTime : Math.floor(0.05 * infectionsByRequestedTime),
+    casesForVentilatorsByRequestedTime : Math.floor(0.02 * infectionsByRequestedTime),
+    dollarsInFlight : infectionsByRequestedTime * avgDailyIncomeInUSD * avgDailyIncomePopulation * timeToElapse,
 
         },
         severeImpact:{
